de7156301cd7cf0489ad289f81187b057794f71e
Osiris <potterosiris@gmail.com>
2021-02-10 21:51:59 -0500

    app use

--- backend/app.js
+++ backend/app.js
@@ -19,3 +19,16 @@ app.use((req, res, next) => {
 
     next();
 });
+
+app.use((req, res, next) => {
+    const error = new HttpError("could not find this route", 404);
+    throw error;
+});
+
+app.use((error, req, res, next) => {
+    if (res.headerSent) {
+        return next(error);
+    }
+    res.status(error.code || 500);
+    res.json({ message: error.message || "an unknown error occured" });
+});
